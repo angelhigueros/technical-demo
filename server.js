dd4dd2d144d513b866c5cfc0ec09f41227c38342
Angel Higueros <angel.higueros01@gmail.com>
2024-07-02 08:04:25 -0600

    fix(): fixed issues with express static

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(cors())
 
 app.use('/api', router)
 
+app.use(express.static(path.join(__dirname, 'client/dist')))
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/dist', 'index.html'))
 })
